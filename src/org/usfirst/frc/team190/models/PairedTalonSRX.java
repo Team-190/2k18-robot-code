3bf291356e066a474ba20795821cbc5ff7c58ee7
Kevin O'Brien <kjrobrien@gmail.com>
2018-01-20 12:43:00 -0500

    Add inital drivetrain subsystem and a paired talon srx class

---
+++ src/org/usfirst/frc/team190/models/PairedTalonSRX.java
@@ -0,0 +1,29 @@
+package org.usfirst.frc.team190.models;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+public class PairedTalonSRX extends TalonSRX {
+	
+	private final TalonSRX follower;
+
+	public PairedTalonSRX(int leaderDeviceNumber, int followerDeviceNumber) {
+		super(leaderDeviceNumber);
+		
+		follower = new TalonSRX(followerDeviceNumber);
+		follower.follow(this);
+	}
+	
+	@Override
+	public void setNeutralMode(NeutralMode neutralMode) {
+		super.setNeutralMode(neutralMode);
+		follower.setNeutralMode(neutralMode);
+	}
+	
+	@Override
+	public void setInverted(boolean invert) {
+		super.setInverted(invert);
+		follower.setInverted(invert);
+	}
+
+}
---
+++ src/org/usfirst/frc/team190/robot/subsystems/Drivetrain.java
@@ -0,0 +1,47 @@
+package org.usfirst.frc.team190.robot.subsystems;
+
+import org.usfirst.frc.team190.models.PairedTalonSRX;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Drivetrain extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	
+	private static final int DEFAULT_TIMEOUT_MS = 10;
+	
+	public static final int LOW_GEAR_PROFILE = 0;
+	public static final int HIGH_GEAR_PROFILE = 1;
+	
+	private PairedTalonSRX leftPair = new PairedTalonSRX(0, 1);
+	private PairedTalonSRX rightPair = new PairedTalonSRX(2, 3);
+
+	public Drivetrain() {
+		this.leftPair.setInverted(false);
+		this.leftPair.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, DEFAULT_TIMEOUT_MS);
+		this.leftPair.setSensorPhase(false);
+		
+		this.rightPair.setInverted(true);
+		this.rightPair.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, DEFAULT_TIMEOUT_MS);
+		this.rightPair.setSensorPhase(true);
+	}
+	
+	public void drive(ControlMode controlMode, double left, double right) {
+		this.leftPair.set(controlMode, left);
+		this.rightPair.set(controlMode, right);
+	}
+	
+	
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
