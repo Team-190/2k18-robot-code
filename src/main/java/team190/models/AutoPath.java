83842a6b88f4dc75a467c8f66530962a30ae3e71
Kevin O'Brien <kjrobrien@gmail.com>
2018-01-25 23:37:43 -0500

    Add some comments and more autos

--- src/main/java/team190/models/AutoPath.java
+++ src/main/java/team190/models/AutoPath.java
@@ -4,20 +4,31 @@ package team190.models;
  * Created by Kevin O'Brien on 1/25/2018.
  */
 public enum AutoPath {
+    // Starting Positions => Scale
     StartLeftScaleLeft,
     StartLeftScaleRight,
     StartRightScaleLeft,
     StartRightScaleRight,
 
+    // Left Scale => Collect and Place Power Cubes
     ScaleLeftCollectCubeOne,
     ScaleLeftPlaceCubeOne,
     ScaleLeftCollectCubeTwo,
     ScaleLeftPlaceCubeTwo,
 
+    // Right Scale => Collect and Place Power Cubes
     ScaleRightCollectCubeOne,
     ScaleRightPlaceCubeOne,
     ScaleRightCollectCubeTwo,
-    ScaleRightPlaceCubeTwo;
+    ScaleRightPlaceCubeTwo,
+
+    // Starting Positions => Switch
+    StartLeftSwitchLeft,
+    StartRightSwitchRight,
+    
+    // Basic Drive Straights
+    ForwardFiveFeet,
+    ForwardTenFeet;
 
     private String directory = "/home/lvuser/Paths";
 
