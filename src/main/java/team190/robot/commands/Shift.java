b7ad2d9108a5da65c54092e9572106c71e36a9e2
Kevin O'Brien <kjrobrien@gmail.com>
2018-01-26 15:48:13 -0500

    Have shifter command finish

--- src/main/java/team190/robot/commands/Shift.java
+++ src/main/java/team190/robot/commands/Shift.java
@@ -1,24 +1,40 @@
 package team190.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
 import team190.robot.Robot;
 import team190.robot.subsystems.Drivetrain.Gear;
-import edu.wpi.first.wpilibj.command.Command;
 
-public class Shift extends Command{
-	Gear gear;
-	public Shift(Gear gear) {
-		gear = this.gear;
-		requires(Robot.drivetrain);
-	}
-	
-	@Override
-	protected void execute () {
-		Robot.drivetrain.shift(gear);
-	}
+public class Shift extends Command {
+    Gear gear;
 
-	@Override
-	protected boolean isFinished() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    public Shift(Gear gear) {
+        gear = this.gear;
+        requires(Robot.drivetrain);
+    }
+
+
+    @Override
+    protected void initialize() {
+        Robot.drivetrain.shift(gear);
+    }
+
+    @Override
+    protected void execute() {
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+        end();
+    }
 
 }
--- src/main/java/team190/robot/subsystems/Drivetrain.java
+++ src/main/java/team190/robot/subsystems/Drivetrain.java
@@ -1,12 +1,10 @@
 package team190.robot.subsystems;
 
-import team190.models.PairedTalonSRX;
-
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import team190.models.PairedTalonSRX;
 import team190.robot.commands.ControllerDriveCommand;
 
 /**
@@ -16,56 +14,52 @@ public class Drivetrain extends Subsystem {
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-	
-	private static final int DEFAULT_TIMEOUT_MS = 10;
-	
-	public static final int LOW_GEAR_PROFILE = 0;
-	public static final int HIGH_GEAR_PROFILE = 1;
-	
-	public static final int SHIFTER_PDM = 0,
-			SHIFTER_PORT = 0;
-	public enum Gear{
-		HIGH, LOW;
-	}
-	public static final boolean SHIFTER_HIGH = true;
-	
-	private PairedTalonSRX leftPair;
-	private PairedTalonSRX rightPair;
 
-	private Solenoid shifter;
-	
-	public Drivetrain() {
-		leftPair = new PairedTalonSRX(0, 1);
-		rightPair = new PairedTalonSRX(2, 3);
-		
-		shifter = new Solenoid(SHIFTER_PDM, SHIFTER_PORT);
-		
-		this.leftPair.setInverted(false);
-		this.leftPair.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, DEFAULT_TIMEOUT_MS);
-		this.leftPair.setSensorPhase(false);
-		
-		this.rightPair.setInverted(true);
-		this.rightPair.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, DEFAULT_TIMEOUT_MS);
-		this.rightPair.setSensorPhase(true);
-	}
-	
-	 public void initDefaultCommand() {
-			setDefaultCommand(new ControllerDriveCommand());
-	 }
-	 
-	 //Shifts gear
-	 public void shift(Gear gear) {
-		 if (gear.equals(Gear.HIGH)) {
-			 shifter.set(SHIFTER_HIGH);
-		 } else if (gear.equals(Gear.LOW)) {
-			 shifter.set(!SHIFTER_HIGH);
-		 }
-	 }
-	
-	public void drive(ControlMode controlMode, double left, double right) {
-		this.leftPair.set(controlMode, left);
-		this.rightPair.set(controlMode, right);
-	}
-  
+    public static final int LOW_GEAR_PROFILE = 0;
+    public static final int HIGH_GEAR_PROFILE = 1;
+    public static final int SHIFTER_PDM = 0,
+            SHIFTER_PORT = 0;
+    private static final int DEFAULT_TIMEOUT_MS = 10;
+    private PairedTalonSRX leftPair;
+    private PairedTalonSRX rightPair;
+    private Solenoid shifter;
+
+    public Drivetrain() {
+        leftPair = new PairedTalonSRX(0, 1);
+        rightPair = new PairedTalonSRX(2, 3);
+
+        shifter = new Solenoid(SHIFTER_PDM, SHIFTER_PORT);
+
+        this.leftPair.setInverted(false);
+        this.leftPair.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, DEFAULT_TIMEOUT_MS);
+        this.leftPair.setSensorPhase(false);
+
+        this.rightPair.setInverted(true);
+        this.rightPair.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, DEFAULT_TIMEOUT_MS);
+        this.rightPair.setSensorPhase(true);
+    }
+
+    public void initDefaultCommand() {
+        setDefaultCommand(new ControllerDriveCommand());
+    }
+
+    //Shifts gear
+    public void shift(Gear gear) {
+        if (gear.equals(Gear.HIGH)) {
+            shifter.set(true);
+        } else if (gear.equals(Gear.LOW)) {
+            shifter.set(false);
+        }
+    }
+
+    public void drive(ControlMode controlMode, double left, double right) {
+        this.leftPair.set(controlMode, left);
+        this.rightPair.set(controlMode, right);
+    }
+
+    public enum Gear {
+        HIGH, LOW;
+    }
+
 }
 
