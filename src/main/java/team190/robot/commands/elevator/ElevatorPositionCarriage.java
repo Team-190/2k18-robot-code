3c06cbcf84644e7e471d5e07712527ae789d0270
Jerry Brown <jbbrown@wpi.edu>
2018-01-30 12:59:21 -0500

    Added elevator position commands.

---
+++ src/main/java/team190/robot/commands/elevator/ElevatorPositionCarriage.java
@@ -0,0 +1,35 @@
+package team190.robot.commands.elevator;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class ElevatorPositionCarriage extends Command {
+
+    public ElevatorPositionCarriage() {
+        // requires...
+    }
+
+    @Override
+    protected void initialize() {
+
+    }
+
+    @Override
+    protected void execute() {
+
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public synchronized void cancel() {
+
+    }
+
+    @Override
+    protected void end() {
+        
+    }
+}
