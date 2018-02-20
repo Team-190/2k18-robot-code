00f3e65272ab815fde5834c2cba901fbfd33a208
Kevin O'Brien <kjrobrien@gmail.com>
2018-02-19 21:01:25 -0500

    Add collector and elevator sequences to StartRightScaleLeft

--- src/main/java/team190/robot/commands/autonomous/StartRightScaleLeft.java
+++ src/main/java/team190/robot/commands/autonomous/StartRightScaleLeft.java
@@ -1,8 +1,13 @@
 package team190.robot.commands.autonomous;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitForChildren;
 import team190.models.AutoSequence;
+import team190.robot.commands.CollectCube;
+import team190.robot.commands.collector.CollectorExtakeFront;
 import team190.robot.commands.drivetrain.DriveSequence;
+import team190.robot.commands.elevator.ElevatorPositionCarriage;
+import team190.robot.commands.elevator.ElevatorPositionHigh;
 
 /**
  * Created by Kevin O'Brien on 1/25/2018.
@@ -10,8 +15,29 @@ import team190.robot.commands.drivetrain.DriveSequence;
 public class StartRightScaleLeft extends CommandGroup {
 
     public StartRightScaleLeft() {
+        // start moving elevator at start of CommandGroup
+        addParallel(new ElevatorPositionHigh());
+        // Drive to the Scale
         addSequential(new DriveSequence(AutoSequence.StartRightScaleLeft));
+        // wait for elevator to be in position
+        addSequential(new WaitForChildren());
+        // Extake the cube
+        addSequential(new CollectorExtakeFront());
+        // Move elevator to carriage height
+        addParallel(new ElevatorPositionCarriage());
+        // Drive to the first cube on the switch
         addSequential(new DriveSequence(AutoSequence.ScaleLeftCollectCubeOne, false));
+        // Wait for elevator to be in position
+        addSequential(new WaitForChildren());
+        // collect the cube and return to carriage height
+        addSequential(new CollectCube());
+        // Move elevator to placement position
+        addParallel(new ElevatorPositionHigh());
+        // Drive to scale
         addSequential(new DriveSequence(AutoSequence.ScaleLeftPlaceCubeOne, false));
+        // Wait for elevator to be in position
+        addSequential(new WaitForChildren());
+        // Extake the cube
+        addSequential(new CollectorExtakeFront());
     }
 }
